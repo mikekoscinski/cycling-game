dd136e6fceeb5d2ca3e35e91b749ed92a1ea7741
Michael Koscinski <michael.koscinski@gmail.com>
2020-06-12 17:27:34 -0400

    Add changes

---
+++ bike-ride.js
@@ -0,0 +1,15 @@
+// Draw images to the canvas continuously
+
+let cvs = document.getElementById('canvas');
+
+let ctx = cvs.getContext('2d');
+
+// Load images
+
+let imageName 	= new Image();
+
+imageName.src 	= "images/"
+
+let audioName 	= new Audio();
+
+audioName.src 	= "audio/cycling-music.mp3"
