6e817bad3b86e3883ed1a66e22caacf15760b052
Mike Koscinski <59475481+mikekoscinski@users.noreply.github.com>
2020-06-15 11:19:52 -0400

    Add side-scroll effect for pokemon sprites

--- bike-ride.js
+++ bike-ride.js
@@ -8,15 +8,18 @@ let ctx = cvs.getContext("2d");
 
 // Load images
 
-let biker 		= new Image();
-let background	= new Image();
-let foreground 	= new Image();
-let magikarp	= new Image();
+let biker 			= new Image();
+let background		= new Image();
+let foreground 		= new Image();
+let magikarp		= new Image();
+let zubat 			= new Image();
 
-biker.src		= "images/male-biker-right.gif";
-background.src	= "images/ocean-background.jpg";
-foreground.src	= "images/ocean-foreground.png";
-magikarp.src	= "images/magikarp.gif";
+biker.src			= "images/male-biker-right.gif";
+background.src		= "images/ocean-background.jpg";
+foreground.src		= "images/ocean-foreground.png";
+magikarp.src		= "images/magikarp.gif";
+zubat.src			= "images/zubat.gif";
+
 
 // Define variables
 
@@ -45,39 +48,39 @@ let gravity = 10;
 
 // Magikarp coordinates
 
-let karp = [];
+let pokemon = [];
 
-karp[0] = {
-	x: cvs.width,
-	y: 0
-}
+pokemon[0] = {
+	x : cvs.width,
+	y : 0
+} 
+
+// Constant for flying pokemon
+
+let constant = 10;
 
 // Draw images
 
 function draw () {
 
 	ctx.drawImage(background, 0, 0); 
+
+	for (let i = 0; i < pokemon.length; i++) {
+		ctx.drawImage(magikarp, pokemon[i].x, pokemon[i].y);
+		ctx.drawImage(zubat, pokemon[i].x, pokemon[i].y + constant)
+
+		pokemon[i].x-= 6;
+
+	}
+
+
 	ctx.drawImage(foreground, 0, 978);
 
 	ctx.drawImage(biker, bikerX, bikerY);
 
 	bikerY += gravity;
 
-	for (let i = 0; i < magikarp.length; i++){
-		ctx.drawImage(magikarp, karp[i].x, pipe[i].y);
-
-		karp[i].x--;
-
-		if (karp[i].x == 125){
-			karp.push({
-				x: cvs.width,
-				y: 100
-			})
-		}
-	}
-
 	requestAnimationFrame(draw);
-
 }
 
 draw();
