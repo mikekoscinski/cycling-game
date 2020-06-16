90faef9bc09ad85a104e63f238a9914c0fdb8f96
Mike Koscinski <59475481+mikekoscinski@users.noreply.github.com>
2020-06-16 14:49:16 -0400

    Prepare to add collision logic

--- bike-ride.js
+++ bike-ride.js
@@ -7,31 +7,30 @@ let ctx = cvs.getContext("2d");
 
 let biker 			= new Image();
 let background		= new Image();
-let foreground 		= new Image();
 let kabutops		= new Image();
 let aerodactyl 		= new Image();
 
 biker.src			= "images/male-biker-right.gif";
 background.src		= "images/ocean-background.jpg";
-foreground.src		= "images/ocean-foreground.png";
 kabutops.src		= "images/kabutops.gif";
 aerodactyl.src		= "images/aerodactyl.gif";
 
 // VARIABLES:
 
+// Set height at which biker cycles
+let cyclingHeight = 525;
+
 // Biker coordinates on the canvas.
 let bikerX 	= 0;
-let bikerY 	= 550;
+let bikerY 	= cyclingHeight;
 
 // Add gravity effect that causes biker to descend post-jump.
 let gravity = 10;
 
 // Gap between walking and flying pokemon
-
 let gap = 150;
 
 // Timer variable
-
 let secs 	= 0;
 let mins 	= 0;
 let hrs 	= 0;
@@ -51,54 +50,49 @@ function moveUp () {
 
 document.addEventListener("keydown", moveUp);
 
-// Pokemon coordinates
-
+// Pokemon coordinates stored in array
 let pokemon = [];
 
 pokemon[0] = {
 	x : cvs.width,
-	y : 910
+	y : 890
 };
 
 // Draw images
-
 function draw () {
 
 	// The scene
-
 	ctx.drawImage(background, 0, 0); 
 
-	ctx.drawImage(foreground, 0, 970);
-
 	// The biker
-
 	ctx.drawImage(biker, bikerX, bikerY);
 
-	bikerY = Math.min(550, bikerY+= gravity);
+	bikerY = Math.min(cyclingHeight, bikerY+= gravity);
 
 	// Timer
-
 	ctx.font = "40px Verdana";
 
 	ctx.fillText("Timer: " + timer, 10, cvs.height - 20);
 
 
-	// The pokemon
-
+	// Continuously draw new Pokemon and add them to the Pokemon array above
 	for (let i = 0; i < pokemon.length; i++) {
 		ctx.drawImage(kabutops,		pokemon[i].x, 	pokemon[i].y 		);
-		ctx.drawImage(aerodactyl, 	pokemon[i].x, 	pokemon[i].y - gap	);
+		//ctx.drawImage(aerodactyl, 	pokemon[i].x, 	pokemon[i].y - gap	);
 
 		pokemon[i].x -= 10;
 
 		if (pokemon[i].x == 1000) {
 			pokemon.push({
 				x : cvs.width,
-				y : 930
+				y : 890
 			});
 		}
 
 		// Detect collision
+
+
+
 
 
 
@@ -117,31 +111,3 @@ function draw () {
 }
 
 draw();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
